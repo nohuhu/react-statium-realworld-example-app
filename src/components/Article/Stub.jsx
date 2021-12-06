a4a6e8eca641ec508cf8b7f5dadae2e1712a0b99
Alex Tokarev <atokarev@clari.com>
2021-12-06 11:58:25 -0800

    More fixes

--- src/components/Article/Stub.jsx
+++ src/components/Article/Stub.jsx
@@ -1,61 +1,57 @@
 import React from 'react';
-import { bind } from 'statium';
+import { useStore } from 'statium';
 import { Link } from 'react-router-dom';
 
 import Userpic from '../Userpic.jsx';
 import Tags from './Tags.jsx';
 import FavIcon from './FavIcon.jsx';
 
-const ArticleStub = ({ slug, title, description, authorUser, authorPic, createdAt, tagList }) => (
-  <div className="article-preview">
-    <div className="article-meta">
-      <Link to={`/@${authorUser}`}>
-        <Userpic src={authorPic} alt={authorUser} />
-      </Link>
+const ArticleStub = () => {
+  const { state: { article } } = useStore();
 
-      <div className="info">
-        <Link className="author" to={`/@${authorUser}`}>
-          {authorUser}
+  const { title, description, slug, tagList, createdAt } = article;
+  const authorUser = article?.author?.username;
+  const authorPic = article.author?.image;
+
+  return (
+    <div className="article-preview">
+      <div className="article-meta">
+        <Link to={`/@${authorUser}`}>
+          <Userpic src={authorPic} alt={authorUser} />
         </Link>
 
-        <span className="date">
-          {new Date(createdAt).toDateString()}
+        <div className="info">
+          <Link className="author" to={`/@${authorUser}`}>
+            {authorUser}
+          </Link>
+
+          <span className="date">
+            {new Date(createdAt).toDateString()}
+          </span>
+        </div>
+
+        <div className="pull-xs-right">
+          <FavIcon slug={slug} />
+        </div>
+      </div>
+
+      <Link to={`/article/${slug}`} className="preview-link">
+        <h1>
+          {title}
+        </h1>
+
+        <p>
+          {description}
+        </p>
+
+        <span>
+          Read more...
         </span>
-      </div>
 
-      <div className="pull-xs-right">
-        <FavIcon slug={slug} />
-      </div>
+        <Tags tags={tagList} component="li" />
+      </Link>
     </div>
+  );
+};
 
-    <Link to={`/article/${slug}`} className="preview-link">
-      <h1>
-        {title}
-      </h1>
-
-      <p>
-        {description}
-      </p>
-
-      <span>
-        Read more...
-      </span>
-
-      <Tags tagList={tagList} />
-    </Link>
-  </div>
-);
-
-const selector = ({ state: { article } }) => ({
-  title: article?.title,
-  description: article?.description,
-  slug: article?.slug,
-  tagList: article?.tagList,
-  authorUser: article?.author?.username,
-  authorPic: article?.author?.image,
-  createdAt: article?.createdAt,
-  favorited: article?.favorited,
-  favoritesCount: article?.favoritesCount,
-});
-
-export default bind(selector)(ArticleStub);
+export default ArticleStub;
--- src/components/Article/Tags.jsx
+++ src/components/Article/Tags.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Tags = ({ tags = [] }) => (
+const Tags = ({ tags = [], component = Link }) => (
   <ul className="tag-list">
-    {tags.map(tag => (
-      <Link key={tag} className="tag-default tag-pill tag-outline"
-        to={`/tag/${tag}`}>
-        {tag}
-      </Link>
-    ))}
+    {tags.map(tag => React.createElement(component, {
+        key: tag,
+        className: "tag-default tag-pill tag-outline",
+        ...component === Link ? { to: `/tag/${tag}` } : {},
+    }, tag))}
   </ul>
 );
 
